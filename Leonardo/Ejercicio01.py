67febf0269d20e0bbb3bd2eece9a6242235d4704
leobormen <leo.borbon.mena@gmail.com>
2026-06-10 16:37:49 -0600

    Revert "Publicacion primeros 6 ejercicios de recursividad"
    This reverts commit 61b406214db0a75c608e0c137205507f2d2936fa.

--- Leonardo/Ejercicio01.py
+++
@@ -1,6 +0,0 @@
-def sumar_digitos(num):
-    if num == 0:
-        return 0
-    else:
-        return num%10 + sumar_digitos(num//10)
-    
--- Leonardo/Ejercicio03.py
+++
@@ -1,9 +0,0 @@
-def mayor_lista(lista, num = 0):
-    if lista == []:
-        return num
-    elif lista[0] > num:
-        return mayor_lista(lista[1:], num=lista[0])
-    else:
-        return mayor_lista(lista[1:], num)
-    
-
